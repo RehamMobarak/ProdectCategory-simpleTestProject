9c9f883e4e6cae3a996b03dafb8b66aef8870948
Reham Mobarak <reham.a.mobarak@gmail.com>
2023-02-22 12:17:37 +0200

    view img in prod show & index

--- resources/views/Products/show.blade.php
+++ resources/views/Products/show.blade.php
@@ -1,30 +1,30 @@
 @extends('layouts.master')
 
 @section('content')
-
     <div class="d-flex justify-content-between mb-4">
         <h3>Show Product</h3>
         <a class="btn btn-success btn-sm" href="{{ route('products.index') }}">List Products</a>
     </div>
+    <img class="img-thumbnail" src="{{ URL::to('/') }}/images/{{ $product->Image }}" height="140" width="140">
 
     <div class="form-group">
         <label>Name</label>
-        <input type="text" name="name" class="form-control" value="{{ $product->Name }}" disabled>
+        <input class="form-control" name="name" type="text" value="{{ $product->Name }}" disabled>
     </div>
     <div class="form-group">
         <label>Color</label>
-        <input type="text" name="color" class="form-control" value="{{ $product->Color }}" disabled>
+        <input class="form-control" name="color" type="text" value="{{ $product->Color }}" disabled>
     </div>
     <div class="form-group">
         <label>Code</label>
-        <input type="number" name="code" class="form-control" value="{{ $product->Code }}" disabled>
+        <input class="form-control" name="code" type="number" value="{{ $product->Code }}" disabled>
     </div>
     <div class="form-group">
         <label>Category</label>
-        <input type="text" name="color" class="form-control" value="{{ $product->category->Name}}" disabled>
+        <input class="form-control" name="color" type="text" value="{{ $product->category->Name }}" disabled>
     </div>
     <div class="form-group">
         <label>Description</label>
-        <textarea name="description" rows="5" placeholder="product description" class="form-control" disabled>{{ $product->Description }}</textarea>
+        <textarea class="form-control" name="description" rows="3" placeholder="product description" disabled>{{ $product->Description }}</textarea>
     </div>
 @endsection
