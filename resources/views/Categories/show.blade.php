73c0cf9d7a968a1aa13fd635ecc17412489bcd2c
Reham Mobarak <reham.a.mobarak@gmail.com>
2023-02-22 16:29:02 +0200

    add condition if a catg doesn't have products don't build a tbl

--- resources/views/Categories/show.blade.php
+++ resources/views/Categories/show.blade.php
@@ -15,34 +15,37 @@
         <label>Description</label>
         <textarea class="form-control" name="description" rows="3" placeholder="category description" disabled>{{ $category->Description }}</textarea>
     </div>
+    @if (count($category->products))
+        <h4>
+            <th>Products</th>
+        </h4>
 
-    <h4>
-        <th>Products</th>
-    </h4>
-
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>Code</th>
-                <th>Color</th>
-                <th>Image</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-
-            @foreach ($category->products as $product)
+        <table class="table table-striped">
+            <thead>
                 <tr>
-                    <td>{{ $product->Name }}</td>
-                    <td>{{ $product->Code }}</td>
-                    <td>{{ $product->Color }}</td>
-                    <td><img class="img-thumbnail" src="{{ URL::to('/') }}/images/{{ $product->Image }}" width="75">
-                    </td>
-                    <td><a class="btn btn-info btn-sm" href="{{ route('products.show', $product->id) }}">Show</a></td>
+                    <th>Name</th>
+                    <th>Code</th>
+                    <th>Color</th>
+                    <th>Image</th>
+                    <th>Action</th>
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
 
-        </tbody>
-    </table>
+                @foreach ($category->products as $product)
+                    <tr>
+                        <td>{{ $product->Name }}</td>
+                        <td>{{ $product->Code }}</td>
+                        <td>{{ $product->Color }}</td>
+                        <td><img class="img-thumbnail" src="{{ URL::to('/') }}/images/{{ $product->Image }}"
+                                width="75">
+                        </td>
+                        <td><a class="btn btn-info btn-sm" href="{{ route('products.show', $product->id) }}">Show</a></td>
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    @endif
+
 @endsection
